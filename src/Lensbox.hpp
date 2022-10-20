99dbf358d4b547fa53ea0bdf42e08827c5abc0d7
ConNoa <constantin.noack@uni-weimar.de>
2022-10-21 00:12:34 +0200

    added Attributes

--- src/Lensbox.hpp
+++ src/Lensbox.hpp
@@ -34,4 +34,21 @@ public:
 private:
   glm::vec3 m_min;
   glm::vec3 m_max;
+
+  glm::vec3 m_center_d0;  //vorderer Scheitelpunkt der Linse
+  glm::vec3 m_center_d1;  //Linsendurchmesser vollständig vordere Linsenseite (manchmal auch H1 ?  Hauptpunkt)
+  glm::vec3 m_center_d2;  //Linsendurchmesser vollständig hintere Linsenseite (manchmal auch H2 ?  Hauptpunkt)
+  glm::vec3 m_center_d3;  //hinterer Scheitelpunkt der Linse
+
+  float m_radius;         //Real-Durchmesser der Linse
+  float m_r1;             //Radius der theoretischen Kugel der vorderen Linsenseite
+  float m_r2;             //Radius der theoretischen Kugel der hinteren Linsenseite
+
+  glm::vec3 m_f1;         //vorderer Brennpunkt
+  glm::vec3 m_f2;         //vorderer Brennpunkt
+
+  glm::vec3 m_Orig;       //Origin -  Mittelpunkt der Linse
+
+  float m_n;              //Brechungsindex der Linse
+
 };
